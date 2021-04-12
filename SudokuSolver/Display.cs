39d499bdb79c02b3734bcb41c9c5ac1bc210750e
Akshat jain <43111888+Akshatjain1999@users.noreply.github.com>
2021-04-13 01:31:13 +0530

    Add files via upload

---
+++ SudokuSolver/Display.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuSolver
+{
+    public class Display
+    {
+        public static void DisplaySudoku(List<StringBuilder> board)
+        {
+            
+            for (var i = 0; i < 9; i++)
+            {
+                for (var j = 0; j < 9; j++)
+                {
+                    Console.Write(board[i][j] + " ");
+                    if (j == 2 || j == 5)
+                    {
+                        Console.Write("|");
+                    }
+                }
+
+                if (i == 2 || i == 5)
+                {
+                    Console.WriteLine("\n------+------+-----");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+            }
+
+        }
+
+    }
+}
